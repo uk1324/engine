aebe3798255db261ab425328d66c0f9a83e524c6
= <83022821+uk1324@users.noreply.github.com>
2024-11-16 11:48:54 +0100

    Debug graphics fix

--- gfx2d/DbgGfx2d.cpp
+++ gfx2d/DbgGfx2d.cpp
@@ -1,16 +1,22 @@
 #include "DbgGfx2d.hpp"
 
 void Dbg::disk(Vec2 pos, f32 radius, Vec3 color) {
+#ifndef FINAL_RELEASE
 	disks.push_back(Disk{ .pos = pos, .radius = radius, .color = color });
+#endif
 }
 
 void Dbg::line(Vec2 e0, Vec2 e1, f32 width, Vec3 color) {
+#ifndef FINAL_RELEASE
 	lines.push_back(Line{ .e0 = e0, .e1 = e1, .width = width, .color = color });
+#endif
 }
 
 void Dbg::update() {
+#ifndef FINAL_RELEASE
 	disks.clear();
 	lines.clear();
+#endif
 }
 
 #ifndef FINAL_RELEASE
